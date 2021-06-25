b7e29aca6b1bff4bc3ecee55ed3e44abc0946e8f
rubenberna <rubenmbernardes@gmail.com>
2021-06-25 12:24:55 +0200

    conditional names

--- template/data-tool/example/src/components/App.js
+++ template/data-tool/example/src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Suspense, lazy, useState } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useNotification } from '../utils/_useNotification';
-import { metadata } from 'data-tool-template'
+import { metadata } from '{{name}}'
 import { Home } from './Home';
 import { SideNav } from './SideNav';
 import { convertToKebabCase } from '../utils/_general.util';
-import 'data-tool-template/dist/index.css'
+import '{{name}}/dist/index.css'
 
-const Component = lazy(() => import('data-tool-template'))
+const Component = lazy(() => import('{{name}}'))
 
 const App = () => {
   const [locale, setLocale] = useState('en')
